dd5c2e6211c662f1a43cefc15d3625b001150c4c
Rajat <rajat804@gmail.com>
2026-03-30 17:34:25 +0530

    shipment routes

--- routes/shipmentRoutes.js
+++ routes/shipmentRoutes.js
@@ -1,12 +1,24 @@
-// routes/shipmentRoutes.js
 import express from 'express';
-import { createShipment, getWarehouses, getVendors, getAllShipments } from '../controllers/shipmentController.js';
+import { 
+  createShipment, 
+  getWarehouses, 
+  getVendors, 
+  getAllShipments,
+  getSingleShipment,
+  updateShipment,
+  deleteShipment
+} from '../controllers/shipmentController.js';
 
 const router = express.Router();
 
-router.get('/',getAllShipments);
+router.get('/', getAllShipments);
 router.get('/warehouses', getWarehouses);
 router.get('/vendors', getVendors);
+
+router.get('/:id', getSingleShipment);   // ✅ Show shipment
+router.put("/:id", updateShipment);
+router.delete('/:id', deleteShipment);   // ✅ Delete shipment
+
 router.post('/', createShipment);
 
 export default router;
