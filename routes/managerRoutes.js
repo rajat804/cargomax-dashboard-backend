96605cbb830e7d3dd702aed8e5eb2d8244fbf7c9
Rajat <rajat804@gmail.com>
2026-03-31 16:37:51 +0530

    manager routes update

--- routes/managerRoutes.js
+++ routes/managerRoutes.js
@@ -1,10 +1,12 @@
 // routes/managerRoutes.js
 import express from 'express';
-import { createManager, getManagers } from '../controllers/managerController.js';
+import { createManager, deleteManager, getManagers, updateManager } from '../controllers/managerController.js';
 
 const router = express.Router();
 
 router.post('/', createManager);
 router.get('/', getManagers);
+router.put('/:id', updateManager);
+router.delete('/:id', deleteManager);
 
 export default router;
