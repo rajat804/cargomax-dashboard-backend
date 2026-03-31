0ff8a173533124d67eeccd782f5b1ee60cb52222
Rajat <rajat804@gmail.com>
2026-03-31 16:37:43 +0530

    manager controller

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -46,3 +46,77 @@ export const getManagers = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+
+
+
+
+// ================= UPDATE =================
+export const updateManager = async (req, res) => {
+
+  try {
+
+    const manager = await Manager.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        returnDocument: "after",
+        runValidators: true
+      }
+    );
+
+    if (!manager) {
+      return res.status(404).json({
+        success: false,
+        message: "Manager not found"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Manager updated successfully",
+      data: manager
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
+
+};
+
+
+
+// ================= DELETE =================
+export const deleteManager = async (req, res) => {
+
+  try {
+
+    const manager = await Manager.findByIdAndDelete(req.params.id);
+
+    if (!manager) {
+      return res.status(404).json({
+        success: false,
+        message: "Manager not found"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Manager deleted successfully"
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
+
+};
