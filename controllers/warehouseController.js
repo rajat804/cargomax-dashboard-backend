901e69fe83c2f61822346636ef1ad4601c842e90
Rajat <rajat804@gmail.com>
2026-03-31 14:20:50 +0530

    ware house controller update

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -16,6 +16,70 @@ export const createWarehouse = async (req, res) => {
   }
 };
 
+export const updateWarehouse = async (req, res) => {
+  try {
+
+    const warehouse = await Warehouse.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        returnDocument: "after",
+        runValidators: true
+      }
+    );
+
+    if (!warehouse) {
+      return res.status(404).json({
+        success: false,
+        message: "Warehouse not found"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Warehouse updated successfully",
+      data: warehouse
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
+};
+
+export const deleteWarehouse = async (req, res) => {
+  try {
+
+    const warehouse = await Warehouse.findByIdAndDelete(req.params.id);
+
+    if (!warehouse) {
+      return res.status(404).json({
+        success: false,
+        message: "Warehouse not found"
+      });
+    }
+
+    res.json({
+      success: true,
+      message: "Warehouse deleted successfully"
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
+};
+
+
+
 export const getWarehouses = async (req, res) => {
   try {
     const warehouses = await Warehouse.find().sort({ createdAt: -1 });
