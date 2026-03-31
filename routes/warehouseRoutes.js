29acfd921cb03ce27e837fdb75e73be8f0c5079e
Rajat <rajat804@gmail.com>
2026-03-31 14:21:02 +0530

    ware house routes update

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createWarehouse, getWarehouses } from '../controllers/warehouseController.js';
+import { createWarehouse, getWarehouses, updateWarehouse, deleteWarehouse } from '../controllers/warehouseController.js';
 
 const router = express.Router();
 
 router.post('/', createWarehouse);
 router.get('/', getWarehouses);
+router.put("/:id", updateWarehouse);
+router.delete("/:id", deleteWarehouse);
 
 export default router;
