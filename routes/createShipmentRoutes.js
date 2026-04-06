a01cbb15618f7c43e5574cc1b7400419f709b8ba
Rajat <rajat804@gmail.com>
2026-04-06 10:59:28 +0530

    create shipment routes update

--- routes/createShipmentRoutes.js
+++ routes/createShipmentRoutes.js
@@ -1,3 +1,4 @@
+// routes/CreateShipmentRoutes.js
 import express from "express";
 import {
   createShipment,
@@ -14,16 +15,15 @@ import {
 
 const router = express.Router();
 
-// Public routes (no authentication required)
 router.post("/create", createShipment);
+router.post("/draft", saveAsDraft);
 router.get("/all", getAllShipments);
-router.get("/stats/all", getShipmentStats);
+router.get("/stats", getShipmentStats);
 router.get("/:id", getShipmentById);
 router.get("/track/:trackingId", trackShipment);
 router.put("/update/:id", updateShipment);
 router.patch("/status/:id", updateShipmentStatus);
 router.delete("/delete/:id", deleteShipment);
-router.post("/draft", saveAsDraft);
 router.get("/customer/:email", getShipmentsByCustomer);
 
 export default router;
